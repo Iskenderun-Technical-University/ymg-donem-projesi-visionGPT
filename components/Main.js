5392c3734c256f5c2c2f21f9c5d6fd5118628441
mert-donmez <mertysta2@gmail.com>
2023-05-08 19:33:24 +0300

    register screen designed

--- components/Main.js
+++ components/Main.js
@@ -8,6 +8,7 @@ import MainUITitle from "./MainUITitle";
 import ChatGPTResponse from "./ChatGPTResponse";
 import GoogleTextDetector from "./GoogleTextDetector";
 import ClearAndSolutionButtons from "./ClearAndSolutionButtons";
+import RegisterScreen from "./RegisterScreen";
 
 
 
