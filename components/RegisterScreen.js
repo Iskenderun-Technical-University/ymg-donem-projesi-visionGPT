ae418aac10b7da320024fb7de1ee4759ed99dc04
mert-donmez <mertysta2@gmail.com>
2023-05-26 01:19:03 +0300

    New start screen

--- components/RegisterScreen.js
+++ components/RegisterScreen.js
@@ -18,15 +18,13 @@ import {
   Inter_600SemiBold,
   Inter_400Regular,
 } from "@expo-google-fonts/inter";
-import BouncyCheckbox from "react-native-bouncy-checkbox";
 import AuthContext from "../context/AuthContext";
-import { MaterialIcons } from '@expo/vector-icons';
-import secretTokens from "../tokens/SecretTokens";
-
+import { MaterialIcons } from "@expo/vector-icons";
+import secretTokens from '../tokens/SecretTokens'
 
 const RegisterScreen = ({ navigation }) => {
-const [isCheckboxChecked,setIsCheckboxChecked] = useState(false);
-const { password, setPassword, email, setEmail, loading,loginOrRegister,handleRegister,loginAnonymously} = useContext(AuthContext);
+  const [isCheckboxChecked, setIsCheckboxChecked] = useState(false);
+  const { loginAnonymously, loading, loggedIn } = useContext(AuthContext);
 
   let [fontsLoaded] = useFonts({
     Inter_900Black,
@@ -39,244 +37,138 @@ const { password, setPassword, email, setEmail, loading,loginOrRegister,handleRe
   if (!fontsLoaded) {
     return null;
   }
-  
 
-  const alertFunc = ()=>{
-    if (isCheckboxChecked === false){
-        alert(secretTokens.termsOfUse);
-        setIsCheckboxChecked(true);
-    }
-    else{
-        setIsCheckboxChecked(false);
-    }
-
-
-  }
-
-  const isAcceptTermsOfUse = ()=>{
-    if (isCheckboxChecked){
-      loginAnonymously();
-    }else{
-      alert('You have to accept terms of use.');
-    }
-  }
-
+  const startLogin = () => {
+    loginAnonymously();
+  };
 
   return (
     <>
-      <StatusBar style="light" />
-      <SafeAreaView style={styles.topWrapper}>
-        <View style={styles.imageAndTitleWrapper}>
-          <Image
-            source={require("../assets/newVisionGPTIcon.png")}
-            style={styles.image}
-          />
-          <Text style={styles.welcomeText}>Hello!</Text>
-        </View>
-      </SafeAreaView>
-      <View style={styles.bottomWrapper}>
+      <StatusBar style="dark" />
         <View style={styles.gradientContainer}>
-          <LinearGradient
-            colors={["white","white"]}
-            style={styles.gradient}
-          >
-            <Text style={styles.registerText}>Register</Text>
-            <Text style={styles.registerTextSub}>Get started free</Text>
-
-            <View style={styles.registerInfoWrapper}>
-
-              <View style={styles.emailTextTitle}>
-              
-                <Text style={styles.emailText}>Email</Text>
-              </View>
-
-              
-                <View style={styles.textInputWrapperEmail}>
-                  <TextInput
-                    inputMode="email"
-                    style={styles.emailInput}
-                    placeholderTextColor={"grey"}
-                    placeholder="Your Email adress"
-                    value={email}
-                    onChangeText={(text)=>setEmail(text)}
-                  />
-                  <MaterialIcons name="mail" style={{marginRight:20}} color={'black'} size={20} />
-                </View>
+          <LinearGradient colors={["#7978FF", "#7149C6","#C689C6","#F94892"]} style={styles.gradient}>
+              {
+                loading ? 
                 
-            
-
-              <View style={styles.emailTextTitle}>
-              
-                <Text style={styles.emailText}>Password</Text>
+                <>
+                <Text style={styles.registerText}>
+                Welcome Again! 
+              </Text>
+              <View style={{justifyContent:'center',alignItems:'center'}}>
+              <Image source={require('../assets/newVisionGPTIcon.png')} style={{width:250,height:250}} />
               </View>
-
-             
-                <View style={styles.textInputWrapperEmail}>
-                  <TextInput
-                    style={styles.emailInput}
-                    secureTextEntry
-                    placeholderTextColor={"grey"}
-                    placeholder="Your password"
-                    value={password}
-                    onChangeText={(text)=>setPassword(text)}
-                  />
-                  <MaterialIcons name="lock" style={{marginRight:20}} color={'black'} size={20} />
-                </View>
+                  <Text style={styles.registerTextSub}>
+                    Just a second...{'\n'}
+                  I am preparing the application for you
+                  </Text>
+                  
+                  </>
                 
                 
-              
-              <View style={styles.checkBox}>
-              <BouncyCheckbox textStyle={{ fontFamily: "Inter_200ExtraLight" }} text='I Accept the Terms of Use' fillColor="purple" onPress={alertFunc}/>
-              </View>
-              <View style={styles.registerButtonWrapper}>
-                <TouchableOpacity style={styles.registerButtonEmail}
-                 onPress={isAcceptTermsOfUse}
-                disabled={false}>
-                    <Text style={styles.registerButtonTextEmail}>Register</Text>
-                    <MaterialIcons name="arrow-forward" color={'white'} size={20} />
-                </TouchableOpacity>
-              </View>
-              <View style={styles.divider}/>
-              
-              
-              <TouchableOpacity style={styles.alreadyHaveAnAccountWrapper} onPress={()=>navigation.navigate('Login')}>
-              <Text style={styles.alreadyHaveAnAccountText}>Already have an account ?</Text>
-              <Text style={styles.alreadyHaveAnAccountTextSingIn}>Login</Text>
-              </TouchableOpacity>
-              
+                : 
+                <>
+              <Text style={styles.registerText}>
+              Welcome
+            </Text>
+            <View style={{justifyContent:'center',alignItems:'center'}}>
+            <Image source={require('../assets/newVisionGPTIcon.png')} style={{width:250,height:250}} />
             </View>
+                <Text style={styles.registerTextSub}>
+                  VisionGPT{"\n"} allows you to provide visual input and engage
+                  in conversations powered by {'\n'}the ChatGPT AI
+                </Text>
+                <View style={styles.registerButtonWrapper}>
+                  <TouchableOpacity
+                    style={styles.registerButtonEmail}
+                    onPress={startLogin}
+                    disabled={false}
+                  >
+                    <Text style={styles.registerButtonTextEmail}>Start </Text>
+
+                    <MaterialIcons
+                      name="arrow-forward-ios"
+                      color={"white"}
+                      size={18}
+                    />
+                  </TouchableOpacity>
+                  <View>
+                      <Text
+                        style={{
+                          fontSize: 10,
+                          textAlign: "center",
+                          color: "black",
+                          fontWeight: "300",
+                          marginTop:10,
+                        }}
+                      >
+                        By clicking 'Start', you agree to our Terms of Use.
+                        We're committed to respecting your rights and privacy.
+                      </Text>
+                      <TouchableOpacity onPress={()=> alert(secretTokens.termsOfUse)} style={{marginBottom:40}}>
+                        <Text style={{
+                          fontSize: 10,
+                          textAlign: "center",
+                          color: "black",
+                          fontWeight: "500",
+                          marginTop:10,
+                          marginBottom:10
+,                          
+                        }}>Terms of Use</Text>
+                      </TouchableOpacity>
+           
+                  </View>
+                </View>
+                </>
+              }
+              
+              
+            
           </LinearGradient>
         </View>
-      </View>
+
     </>
   );
 };
 
 const styles = StyleSheet.create({
-    checkBox:{
-        marginTop:15,
-        marginLeft:'10%'
-    },
-    alreadyHaveAnAccountWrapper:{
-      flexDirection:'row',
-      justifyContent:'center',
-      alignItems:'center',
-      marginTop:20,
-    },
-    googleButtonGradient:{
-        borderRadius:10,
-        elevation:10,
-        
-    },
-    continueWithText:{
-        color: "black",
-        textAlign: "center",
-        
-        fontSize: 18,
-        fontFamily: "Inter_200ExtraLight",
-        marginBottom:20,
-    },
-    alreadyHaveAnAccountText:{
-        color: "black",
-        textAlign: "center",
+ 
 
-        fontSize: 16,
-        fontFamily: "Inter_200ExtraLight",
-        marginTop:15,
-       
-    },
-    alreadyHaveAnAccountTextSingIn:{
-      color: "black",
-      textAlign: "center",
-      marginLeft: 16,
-      fontSize: 18,
-      fontFamily: "Inter_400Regular",
-      marginTop:15,
-    },
+  registerButtonWrapper: {
+    marginHorizontal: 40,
     
-    divider:{
-        height: StyleSheet.hairlineWidth,
-        backgroundColor:'black',
-        marginTop:50,
-        marginHorizontal:70,
-        marginBottom:10,
-    },
-  gradientButton: {
-    alignItems: "center",
-    justifyContent: "center",
-    borderRadius: 10,
-    height:40,
-    width:'100%'
-  },
-  registerButtonWrapper:{
-    marginHorizontal:40,
-    marginTop:20,
   },
   registerButtonEmail: {
-    backgroundColor: "rgba(115,114,253,1)",
-    flexDirection:'row',
-    justifyContent:'center',
-    alignItems:'center',
-    height:40,
+    backgroundColor: "#52006A",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    height: 40,
     borderRadius: 10,
   },
-  bottomWrapper: {
-    flex: 1,
-    backgroundColor: "black",
-  },
+
   registerButtonTextEmail: {
     color: "white",
     fontSize: 22,
     fontFamily: "Inter_400Regular",
   },
-  
-  inputWrapper: {
-    flexDirection: "row",
-  },
-  inputEmailIcon: {
-    height: 20,
-    width: 20,
-    marginRight:5,
-  },
-  emailTextTitle: {
-    marginLeft: "15%",
-    marginTop: 20,
-    marginBottom: 5,
-    flexDirection:'row',
-  },
-  emailInput:{
-    height:60,
-    marginHorizontal:20,
-},
-  
-  textInputWrapperEmail:{  
-    backgroundColor:'#DDE6ED',
-    height:45,
-    marginHorizontal:40,
-    borderRadius:15,
-    flexDirection:'row',
-    justifyContent:'space-between',
-    alignItems:'center'
-},
+
+
+
   registerTextSub: {
-    color: "black",
+    color: "#321E1E",
     textAlign: "center",
-    marginBottom:40,
-    fontSize: 18,
-    fontFamily: "Inter_200ExtraLight",
+    marginBottom: 20,
+    marginHorizontal: 16,
+    fontSize: 24,
+    fontFamily: "Inter_900Black",
   },
   registerText: {
     color: "black",
     textAlign: "center",
-
-    marginTop: 16,
-    fontSize: 36,
-    fontFamily: "Inter_300Light",
+    fontSize: 50,
+    fontFamily: "Inter_900Black",
   },
-  image: {
-    height: 200,
-    width: 200,
-  },
+  
   emailText: {
     color: "black",
     fontFamily: "Inter_200ExtraLight",
@@ -293,14 +185,12 @@ const styles = StyleSheet.create({
   },
   gradientContainer: {
     flex: 1,
-    borderTopLeftRadius: 30,
-    borderTopRightRadius: 30,
     overflow: "hidden",
   },
   gradient: {
     flex: 1,
+    justifyContent:'center',
   },
-
   welcomeText: {
     fontSize: 60,
     fontFamily: "Inter_200ExtraLight",
